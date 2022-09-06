5d1020b64eb7b9d7e77659f0fbe476f1adb9f88a
Halo <1654403734@qq.com>
2022-09-06 18:56:39 +0800

    remove: 删除临时文件

--- 1.js
+++
@@ -1,8 +0,0 @@
-const glob = require('glob')
-const fs = require('fs-extra')
-
-for (const p of glob.sync('create-*', { cwd: 'packages' })) {
-    const pkg = fs.readJsonSync(`packages/${p}/package.json`, { encoding: 'utf-8' })
-    const { description } = pkg
-    console.log(`yarn cp -n ${p} -d "${description}"`)
-}
